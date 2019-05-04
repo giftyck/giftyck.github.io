822547e37ed513ebac6497cf56015146335a464c
luis <luiscastro193@gmail.com>
2019-05-04 16:17:05 +0200

    Real recommendations

--- components/elements/items.js
+++ components/elements/items.js
@@ -1,26 +1,28 @@
 "use strict";
 async function recommendedElement() {
-	let container = htmlToElement(`<div id="product-board" class="flex"></div>`);
+	let container = htmlToElement(`<div id="product-board" class="row"></div>`);
 	let items = await recommendedItems.value;
 	
 	for (let i = 0; i < items.length; i++) {
 		let item = items[i];
 		
 		container.appendChild(htmlToElement(`<div class="col s12 m3 ${i != 0 && "hide-on-small-only" || ''}">
-		     <div class="card hoverable">
-		         <div class="card-image">
-		             <img src="static/img/${item.img}.jpg">
-		         </div>
-		         <div class="card-content">
-		             <span class="card-title">${item.name}</span>
-		             <p>${item.description}</p>
-		         </div>
-		         <div class="card-action center-align">
-		             <a onclick="ratings.set(${item.id}, 0)" class="btn red waves-effect">Nop!</a>
-		             <a onclick="ratings.set(${item.id}, 1)" class="btn waves-effect">Puede ser!</a>
-		         </div>
-		     </div>
-		</div>`));
+           <div class="card hoverable">
+               <div class="card-image">
+                   <img src="static/img/${item.img}.jpg">
+               </div>
+               <div class="card-content">
+                   <span class="card-title">${item.name}</span>
+                   <p>${item.description}</p>
+               </div>
+               <div class="card-action center-align">
+                   <a onclick="ratings.set(${item.id}, 0)" class="btn-large red waves-effect">No</a>
+                   <a onclick="recommendedItems.remove(${i})" class="btn-large blue waves-effect">??</a>
+                   <a onclick="ratings.set(${item.id}, 1)" class="btn-large waves-effect">Si</a>
+
+               </div>
+           </div>
+      </div>`));
 	}
 	
 	return container;
--- components/items.js
+++ components/items.js
@@ -14,7 +14,7 @@ function importItems() {
 }
 
 window.items = importItems();
-window.recommendationMode = newObservable();
+window.recommendationMode = newObservable("consolidated");
 
 function shuffle(array) {
 	for (let i = array.length - 1; i > 0; i--) {
@@ -27,12 +27,44 @@ function shuffle(array) {
 
 async function randomItems() {
 	let randItems = [...await items];
-	return shuffle(randItems).slice(0, 8);
+	return shuffle(randItems).slice(0, 10);
+}
+
+function getRecommendedIds(ratings, mode) {
+	return new Promise((resolve, reject) => {
+		if (!ratings.size)
+			return resolve([]);
+		
+		let queryString = `?mode=${mode}&`;
+		
+		for (let [key, value] of ratings)
+			queryString += `${key}=${value}&`;
+		
+		let request = new XMLHttpRequest();
+		request.open("GET", 'https://giftyck-engine.herokuapp.com/recommend' + queryString);
+		request.responseType = "json";
+		request.onload = () => {
+			if (request.status < 400) {
+				resolve(request.response);
+			}
+			else reject(request.statusText);
+		};
+		request.onerror = () => reject(request.statusText);
+		request.send();
+	});
 }
 
 async function getRecommendedItems(ratings, mode) {
-	console.log(ratings);
-	return randomItems();
+	let all_items = await items;
+	
+	let recommended = (await getRecommendedIds(ratings, mode)).map(item => {
+		return all_items[item._id - 1];
+	});
+	
+	if (recommended.length < 1)
+		recommended = randomItems();
+	
+	return recommended;
 }
 
 window.recommendedItems = newObservable(randomItems());
@@ -42,3 +74,13 @@ recommendationMode.subscribe(document, () => {
 ratings.subscribe(document, () => {
 	recommendedItems.value = getRecommendedItems(ratings.value, recommendationMode.value);
 });
+
+recommendedItems.remove = function(index) {
+	async function newRecommended() {
+		let recommended = await recommendedItems.value;
+		recommended.splice(index, 1);
+		return recommended;
+	}
+	
+	recommendedItems.value = newRecommended();
+}
